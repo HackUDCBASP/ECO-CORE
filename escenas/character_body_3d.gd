e0360236aa387c6d389df71a73a4598c40891a22
SheepBomber <adrian.formigo@udc.es>
2026-02-28 19:02:52 +0100

    feat: añadir el modelo animado

--- escenas/character_body_3d.gd
+++ escenas/character_body_3d.gd
@@ -4,28 +4,27 @@ const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
 @onready var camera = $SpringArm3D/Camera3D
-@onready var model = $Node3D
+@onready var model = $repoandando
+@onready var anim_player = $repoandando/AnimationPlayer
+
+func _process(delta: float) -> void:
+	$repoandando/AnimationPlayer.play("Esqueleto_accion")
 
 func _physics_process(delta: float) -> void:
-	# Gravedad
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Salto
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Input
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var cam_basis = camera.global_transform.basis
 	var forward = -cam_basis.z
 	var right = cam_basis.x
-
 	forward.y = 0
 	right.y = 0
 	forward = forward.normalized()
 	right = right.normalized()
-
 	var direction = (right * input_dir.x + forward * -input_dir.y)
 
 	if direction.length() > 0.1:
@@ -37,6 +36,11 @@ func _physics_process(delta: float) -> void:
 
 	move_and_slide()
 
-	# Rotar el modelo hacia la dirección del movimiento
 	if direction.length() > 0:
 		model.look_at(model.global_transform.origin - direction, Vector3.UP)
+
+	# Animaciones
+	if direction.length() > 0.1 and is_on_floor():
+		anim_player.play("repoandando")
+	else:
+		pass #anim_player.play("repoquieto")
--- escenas/scanf.gd
+++ escenas/scanf.gd
@@ -12,7 +12,6 @@ signal dialog_started
 signal dialog_ended
 
 func _ready() -> void:
-	body_entered.connect(_on_body_entered)
 	body_exited.connect(_on_body_exited)
 
 func _on_body_entered(body: Node3D) -> void:

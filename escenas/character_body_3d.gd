0489d558571fff02be4c7e05a837480a1a58e280
SheepBomber <adrian.formigo@udc.es>
2026-02-28 10:41:36 +0100

    feat: Mejorar cámara del jugador

--- escenas/character_body_3d.gd
+++ escenas/character_body_3d.gd
@@ -1,24 +1,36 @@
 extends CharacterBody3D
 
-
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
+@onready var camera = $SpringArm3D/Camera3D
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# Gravedad
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
+	# Salto
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Input
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
+
+	# dirección relativa a la cámara
+	var cam_basis = camera.global_transform.basis
+	var forward = -cam_basis.z  # Hacia donde mira la cámara
+	var right = cam_basis.x
+
+	#aplanamos los vectores para que el personaje no vuele/se hunda
+	forward.y = 0
+	right.y = 0
+	forward = forward.normalized()
+	right = right.normalized()
+
+	var direction = (right * input_dir.x + forward * -input_dir.y)
+
+	if direction.length() > 0.1:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
